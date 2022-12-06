0a3970ecf0599e89d8c17e2c88a68d167f2f36a3
yooveloper <yooveloper@gmail.com>
2022-12-06 22:59:19 +0900

    add: products component

---
+++ pages/products.tsx
@@ -0,0 +1,106 @@
+import Carousel from 'nuka-carousel'
+import Image from 'next/image'
+import { useState } from 'react'
+import Head from 'next/head'
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1016/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1016/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1013/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1013/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1012/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1012/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1011/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1011/250/150/',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/4v.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/4v.jpg',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/1.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/1.jpg',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/2.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/2.jpg',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+]
+
+export default function Products() {
+  const [index, setIndex] = useState(0)
+  return (
+    <>
+      <Head>
+        <meta
+          property="og:url"
+          content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
+        />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:title"
+          content="When Great Minds Don’t Think Alike"
+        />
+        <meta
+          property="og:description"
+          content="How much does culture influence creative thinking?"
+        />
+        <meta
+          property="og:image"
+          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
+        />
+      </Head>
+      <Carousel
+        animation="fade"
+        autoplay={false}
+        withoutControls
+        wrapAround
+        speed={10}
+        slideIndex={index}
+      >
+        {images.map((item) => (
+          <Image
+            key={item.original}
+            src={item.original}
+            alt="image"
+            width={1000}
+            height={600}
+            layout="responsive"
+          />
+        ))}
+      </Carousel>
+      <div style={{ display: 'flex' }}>
+        {images.map((item, index) => (
+          <div key={index} onClick={() => setIndex(index)}>
+            <Image src={item.original} alt="image" width={100} height={60} />
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
