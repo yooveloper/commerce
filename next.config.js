a60923fb7eb0be2b54eb5e2a1854168a71f59e5d
yooveloper <yooveloper@gmail.com>
2022-12-06 22:58:41 +0900

    add: emotion

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    emotion: true,
+  },
+  images: {
+    domains: ['picsum.photos', 'raw.githubusercontent.com'],
+  },
 }
 
 module.exports = nextConfig
